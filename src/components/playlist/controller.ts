b758dcff82d17f0bfcb3185ea83ed1b519fa8f0e
everyzzz <jquisper2@gmail.com>
2023-01-22 01:48:12 -0500

    feat/7-create-folder-datasource

--- src/components/playlist/controller.ts
+++ src/components/playlist/controller.ts
@@ -1,7 +1,5 @@
 import { Request, Response } from "express";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import prisma from "../../datasource/";
 
 export const findPlaylists = async (_req: Request, res: Response): Promise<void> => {
 	try{
