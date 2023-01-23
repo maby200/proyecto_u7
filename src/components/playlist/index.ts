5f3e2e3201fcfaaeaad728e16a53a59785097ab6
everyzzz <jquisper2@gmail.com>
2023-01-23 16:33:06 -0500

    ENH/7.2-create-route-findId

--- src/components/playlist/index.ts
+++ src/components/playlist/index.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { findPlaylists, addPlaylist, addSongToPlaylist } from "./controller";
+import { findPlaylists, addPlaylist, addSongToPlaylist, findPlaylistsById } from "./controller";
 const playlistRouter: Router = Router();
 
 playlistRouter.get("/", findPlaylists);
+playlistRouter.get("/:id", findPlaylistsById);
 playlistRouter.post("/", addPlaylist);
 playlistRouter.put("/addsong", addSongToPlaylist);
 
